843b2e0a6a6292fad5c67caa9a604eea390a9226
Meryammmm <smairem1810@gmail.com>
2024-06-02 10:33:52 +0100

    first push

---
+++ movie-app/src/Movies.js
@@ -0,0 +1,24 @@
+const Movies =[
+    {
+    title:'The maze runnner',
+    description:' Thomas wakes up trapped in a massive, ever-changing maze with a group of boys who have no memory of the outside world. Together, they must navigate the maze, face deadly creatures, and uncover the truth about their situation to find a way to escape.',
+    posterURL:'https://musicart.xboxlive.com/7/f3214e00-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080',
+    rating:'10'
+    },
+
+    {
+    title:'The hunger games',
+    description:' Katniss Everdeen volunteers to compete in a televised death match in a dystopian future, fighting for survival and challenging a totalitarian regime.',
+    posterURL:'https://toutelaculture.com/wp-content/uploads/2014/11/unger-567x854.jpg',
+    rating:'9'
+    },
+
+
+    {
+    title:'The divergent',
+    description:' "Divergent" follows Tris Prior, who discovers she is Divergent in a society divided by virtues, and uncovers a plot to eliminate those like her.',
+    posterURL:'https://m.media-amazon.com/images/M/MV5BMTgxOTYxMTg3OF5BMl5BanBnXkFtZTgwMDgyMzA2NDE@._V1_.jpg',
+    rating:'9'
+    }
+]
+export default Movies;
